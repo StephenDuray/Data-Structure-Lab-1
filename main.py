cda6ea21e3ba582ae597311b40447fff40e6660d
StephenDuray <Stepheenduray@gmail.com>
2024-09-04 18:45:57 +0800

    Laboratory 3

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-NameOne = input("Enter Name: ")
-NameTwo = input("Enter Name: ")
-NameThree = input("Enter Name: ")
-result = ("Your Classmates are: " + NameOne + ", " + NameTwo + " & " + NameThree)
-print(result)
-FirstNumber = int(input("Enter Number One: "))
-SecondNumber = int(input("Enter Number Two: "))
-if FirstNumber < SecondNumber:
-    print(str(FirstNumber) + " is Less than " + str(SecondNumber))
-elif FirstNumber > SecondNumber:
-    print(str(FirstNumber)+ " is Greater Than " + str(SecondNumber))
-else:
-    print(str(FirstNumber) + " is Equal to " + str(SecondNumber))
+def ask_user():
+    try:
+        return int(input("Enter a number: "))
+    except ValueError:
+        return ask_user()
+
+
+def ask_fruit():
+    fruits = input("Enter A for Apple and B for Banana: ")
+    if fruits == "A":
+        return "Apple"
+    elif fruits == "B":
+        return "Banana"
+    else:
+        return ask_fruit()
+
+
+display = ask_user()
+container = []
+for i in range(display):
+    container.append(ask_fruit())
+
+if len(container) > 1:
+
+    FinalResult = ", ".join(container[:-1]) + ", " + "and " + container[-1]
+
+    print(f"You have {FinalResult} on your list.")

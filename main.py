5d099e532e73176282e06e1ab0256aa19e03ff30
StephenDuray <Stepheenduray@gmail.com>
2024-11-12 15:30:53 +0800

    Prefinal Laboratory

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
-def ask_user():
-    try:
-        return int(input("Enter a number: "))
-    except ValueError:
-        return ask_user()
+students = {}
 
+for i in range(1, 4):
+    student_number = input(f"Enter student number for classmate {i}: ")
+    first_name = input(f"Enter first name for classmate {i}: ")
+    students[student_number] = first_name
 
-def ask_fruit():
-    fruits = input("Enter A for Apple and B for Banana: ")
-    if fruits == "A":
-        return "Apple"
-    elif fruits == "B":
-        return "Banana"
-    else:
-        return ask_fruit()
+print("\nOriginal Student List:")
+for student_number, first_name in students.items():
+    print(f"Student Number: {student_number}, First Name: {first_name}")
 
+if len(students) >= 3:
+    third_key = list(students.keys())[2]
+    students.pop(third_key)
+else:
+    print("Not enough entries to remove the third one.")
 
-display = ask_user()
-container = []
-for i in range(display):
-    container.append(ask_fruit())
+my_student_number = input("\nEnter your student number: ")
+my_first_name = input("Enter your first name: ")
+students[my_student_number] = my_first_name
 
-if len(container) > 1:
-
-    FinalResult = ", ".join(container[:-1]) + ", " + "and " + container[-1]
-
-    print(f"You have {FinalResult} on your list.")
+print("\nUpdated Student List:")
+for student_number, first_name in students.items():
+    print(f"Student Number: {student_number}, First Name: {first_name}")
